ba20372fbc093771d583cb0c6f3e97440102f55c
github username <github email address>
2018-10-12 15:03:39 -0400

    Relationships have been made

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_many :images
+  has_many :shopping_carts
+  has_many :users, through: :shopping_carts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :shopping_carts
+  has_many :products, through: :shopping_carts
 end
