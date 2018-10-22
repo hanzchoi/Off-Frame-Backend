e687860ff0c8914bfdff5c5e742bb1a41ae3e64f
github username <personabe1984@gmail.com>
2018-10-22 13:21:35 -0400

    Added more data for the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 #cloudnary_url = "http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/"
 
 Product.create(
-  name: 'Wide Leg Cropped Trouser v1',
+  name: 'Wide Leg Cropped Trouser - Prussion Blue',
   price: 89.99,
   color: 'Prussion Blue',
   category: 'Trouser',
   size: 'EU 42, WAIST MEASUREMENT: 80~100, FULL HIP MEASUREMENT : 120, RISER MEASUREMENT: 32, PANTS LENGTH: 81',
-  description: 'Testing Testing')
+  description: 'Model Size: 178cm / 76kg / EU size 42')
 
 # Wide Leg Cropped Trouser Prussion Blue #Id 1
 Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/P-001B-1.jpg', product_id: 1)
@@ -14,43 +14,206 @@ Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/
 Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/P-001B-3.jpg', product_id: 1)
 
 Product.create(
-  name: 'Wide Leg Cropped Trouser v2',
+  name: 'Wide Leg Cropped Trouser - Grey Brown',
   price: 89.99,
   color: 'Grey Brown',
   category: 'Trouser',
   size: 'EU 42, WAIST MEASUREMENT: 80~100, FULL HIP MEASUREMENT : 120, RISER MEASUREMENT: 32, PANTS LENGTH: 81',
-  description: 'Testing Testing')
+  description: 'Model Size: 178cm / 76kg / EU size 42')
 
+#Wide Leg Cropped Trouser v2
 Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/P-001A-1.jpg', product_id: 2)
 Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/P-001A-2.jpg', product_id: 2)
 
+Product.create(
+  name: 'Fifi Dress'
+  price: 139.99,
+  color: 'Pastel Blue',
+  category: 'Dress',
+  size: 'EU40-42, CENTER BACK: 88, CENTER FRONT : 78, BUST MEASUREMENT: 120, WAIST MEASUREMENT: 140, FULL HIP MEASUREMENT: 122',
+  description: 'Model Size: 178cm / 76kg / EU size 42'
+)
 
-# T-001-1.jpg
-# T-001-2.jpg
-# T-001-3.jpg
-# T-001-4.jpg
+#Fifi Dress
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/SD-002-1.jpg', product_id: 3);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/SD-002-2.jpg', product_id: 3);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/SD-002-3.jpg', product_id: 3);
+
+
+Product.create(
+  name: 'Honey Top',
+  price: 89.99 ,
+  color: 'Bright Yellow',
+  category: 'Top',
+  size: '(EU40-46), CENTER BACK: 69, CENTER FRONT : 61, BUST MEASUREMENT: 118, WAIST MEASUREMENT: 138, FULL HIP MEASUREMENT: 140, SHOULDER LENGTH: 31',
+  description: 'Model Size: 178cm / 76kg / EU size 42'
+)
+
+# Honey Top
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-001-1.jpg', product_id: 4);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-001-2.jpg', product_id: 4);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-001-3.jpg', product_id: 4);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-001-4.jpg', product_id: 4);
+
+Product.create(
+  name: 'Button Button Shirt',
+  price: 122.99,
+  color: 'White',
+  category: 'Top',
+  size: '(EU42), CENTER BACK: 79, CENTER FRONT: 68, BUST MEASUREMENT: 124, WAIST MEASUREMENT: 130, FULL HIP MEASUREMENT: 140, CROSS BACK / FRONT MEASUREMENT: 108, SHOULDER LENGTH: 20',
+  description: 'Model Size: 178cm / 76kg / EU size 42'
+)
+
+# Button Button Shirt
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/052448134.jpg', product_id: 5);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/S-001-1.jpg', product_id: 5);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/S-001-2.jpg', product_id: 5);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/S-001-3.jpg', product_id: 5);
+
+
+Product.create(
+  name: 'Side Shoulder Ribbon',
+  price: 80.99,
+  color: 'Navy Blue',
+  category: 'Top',
+  size: '(EU44-46), CENTER BACK: 54, CENTER FRONT : 54, BUST MEASUREMENT: 116, WAIST MEASUREMENT: 120, FULL HIP MEASUREMENT: 135',
+  description: 'Model Size: 178cm / 76kg / EU size 42'
+)
+
+#Side Shoulder Ribbon
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-004-1.jpg', product_id: 6);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-004-2.jpg', product_id: 6);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-004-3.jpg', product_id: 6);
+
+
+Product.create(
+  name: 'Fifi Top - Lavender Blue',
+  price: 103.99,
+  color: 'LAVENDER BLUE',
+  category: 'Top',
+  size: '(EU42), GARMENT LENGTH FRONT: 68, GARMENT LENGTH BACK: 75, BUST MEASUREMENT: 116, WAIST MEANSUREMENT: 120, FULL HIP MEASUREMENT: 135, SLEEVE LENGTH: 30',
+  description: 'Model Size: 178cm / 76kg / EU size 42'
+)
+
+# Fifi Top - Lavender Blue
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-005B-1.jpg', product_id: 7);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-005B-2.jpg', product_id: 7);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-005B-3.jpg', product_id: 7);
+
+
+
+Product.create(
+  name: 'Fifi Top - White',
+  price: 103.99,
+  color: 'White',
+  category: 'Top',
+  size: '(EU42), GARMENT LENGTH FRONT: 68, GARMENT LENGTH BACK: 75, BUST MEASUREMENT: 116, WAIST MEANSUREMENT: 120, FULL HIP MEASUREMENT: 135, SLEEVE LENGTH: 30',
+  description: 'Model Size: 178cm / 76kg / EU size 42'
+)
+
+# Fifi Top - White
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-005A-1.jpg', product_id: 8);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-005A-2.jpg', product_id: 8);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-005A-3.jpg', product_id: 8);
+
+
+
+Product.create(
+  name: 'V Neck T-Shirt - Navy Blue',
+  price: 31.99,
+  color: 'Navy Blue',
+  category: 'Top',
+  size: '(EU40-42), CENTER BACK: 68, CENTER FRONT 50, BUST MEASUREMENT: 110, WAIST MEASUREMENT: 104, FULL HIP MEASUREMENT: 104, CROSS BACK MEASUREMENT: 88, CROSS FRONT MEASUREMENT: 88',
+  description: 'Model Size: 178cm / 76kg / EU size 42'
+)
+
+# V Neck T-Shirt - Navy Blue
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-006-1.jpg', product_id: 9);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-006-2.jpg', product_id: 9);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-006-3.jpg', product_id: 9);
+
+
+Product.create(
+  name: 'Box Pleat Top',
+  price: 90.99,
+  color: 'White',
+  category: 'Top',
+  size: '(EU40-42), CENTER BACK: 58, CENTER FRONT : 58, BUST MEASUREMENT: 116, WAIST MEASUREMENT: 118, FULL HIP MEASUREMENT: 140',
+  description: 'Model Size: 178cm / 76kg / EU size 42'
+)
+
+# Box Pleat Top
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-002-1.jpg', product_id: 10);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-002-2.jpg', product_id: 10);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-002-3.jpg', product_id: 10);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/T-002-4.jpg', product_id: 10);
+
+
+Product.create(
+  name: 'DESSERT DRESS',
+  price: 135.99,
+  color: 'Off White',
+  category: 'Dress',
+  size: '(EU40-42), CENTER BACK: 94, CENTER FRONT : 86, BUST MEASUREMENT: 116, WAIST MEASUREMENT: 116, FULL HIP MEASUREMENT: 116, SLEVE LENGTH: 35, NECKLINE MEASUREMENT: 68',
+  description: 'Model Size: 178cm / 76kg / EU size 42'
+)
+
+# Dessert Dress
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/SD-003-1.jpg', product_id: 11);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/SD-003-2.jpg', product_id: 11);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/SD-003-3.jpg', product_id: 11);
+Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/SD-003-4.png', product_id: 11);
+
 #
-# T-002-1.jpg
-# T-002-2.jpg
-# T-002-3.jpg
-# T-002-4.jpg
+# Product.create(
+#   name: '',
+#   price: ,
+#   color: '',
+#   category: '',
+#   size: '',
+#   description: ''
+# )
 #
-# T-004-1.jpg
-# T-004-2.jpg
-# T-004-3.jpg
 #
-# T-005A-1.jpg
-# T-005A-2.jpg
-# T-005A-3.jpg
+# Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/', product_id: 12);
 #
-# T-005B-1.jpg
-# T-005B-2.jpg
-# T-005B-3.jpg
+# Product.create(
+#   name: '',
+#   price: ,
+#   color: '',
+#   category: '',
+#   size: '',
+#   description: ''
+# )
 #
-# T-006-1.jpg
-# T-006-2.jpg
-# T-006-3.jpg
 #
+# Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/', product_id: 13);
+#
+# Product.create(
+#   name: '',
+#   price: ,
+#   color: '',
+#   category: '',
+#   size: '',
+#   description: ''
+# )
+#
+#
+# Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/', product_id: 14);
+#
+# Product.create(
+#   name: '',
+#   price: ,
+#   color: '',
+#   category: '',
+#   size: '',
+#   description: ''
+# )
+# 
+#
+# Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/off-frame-studio/', product_id: 15);
+
+
 # T-007-1.png
 # T-007-2.jpg
 # T-007-3.jpg
@@ -59,29 +222,10 @@ Image.create(url: 'http://res.cloudinary.com/dbgp0ijfb/image/upload/v1539268407/
 # T-008-2.jpg
 # T-008-3-NO.png
 #
-# S-001-1.jpg
-# S-001-2.jpg
-# S-001-3.jpg
-#
-# SD-002-1.jpg
-# SD-002-2.jpg
-# SD-002-3.jpg
-#
 # SD-004A-1.jpg
 # SD-004A-2.jpg
 # SD-004A-3.png
 #
-# SD-003-1.jpg
-# SD-003-2.jpg
-# SD-003-3.jpg
-# SD-003-4.png
-#
 # SD-004B-1.jpg
 # SD-004B-2.jpg
 # SD-004B-3.png
-#
-
-#
-#
-#
-# 052448134.jpg
